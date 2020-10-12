8eeafc4df9eaf0aa84e3b4ffa641f30b88d520fb
Felipe César <felipecesr@gmail.com>
2020-10-12 13:54:05 -0300

    init application

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,44 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { useEffect, useState } from 'react';
+import netlifyAuth from '../netlifyAuth.js';
 
 export default function Home() {
+  const [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    netlifyAuth.initialize((user) => {
+      setLoggedIn(!!user);
+      setUser(user);
+    });
+  }, [loggedIn]);
+
+  const login = () => {
+    netlifyAuth.authenticate((user) => {
+      setLoggedIn(!!user);
+      setUser(user);
+      netlifyAuth.closeModal();
+    });
+  };
+
+  const logout = () => {
+    netlifyAuth.signout(() => {
+      setLoggedIn(false);
+      setUser(null);
+    });
+  };
+
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-
-      <style jsx>{`
-        .container {
-          height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
-            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+    <>
+      {loggedIn ? (
+        <div>
+          You are logged in!
+          {user && <>Welcome {user?.user_metadata.full_name}!</>}
+          <br />
+          <button onClick={logout}>Log out here.</button>
+        </div>
+      ) : (
+        <button onClick={login}>Log in here.</button>
+      )}
+    </>
+  );
 }
